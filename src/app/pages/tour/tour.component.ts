188fcf46870463a13d07e2f78b93d790ee366748
NgocMinhnn <61500935+NgocMinhnn@users.noreply.github.com>
2021-12-25 04:15:40 +0700

    update add tour

--- src/app/pages/tour/tour.component.ts
+++ src/app/pages/tour/tour.component.ts
@@ -4,6 +4,9 @@ import CustomStore from "devextreme/data/custom_store";
 import {TourLoaiService} from "../../services/tour-loai.service";
 import {Tour_Loai} from "../../shared/model/Tour_Loai";
 import {Tour} from "../../shared/model/Tour";
+import DevExpress from "devextreme";
+import data = DevExpress.data;
+import {TourChiTietService} from "../../services/tour-chi-tiet.service";
 
 @Component({
   selector: 'app-tour',
@@ -19,14 +22,15 @@ export class TourComponent implements OnInit {
   LoaiTour : Tour_Loai[] = [];
 
   tour :Tour[] = [];
-  constructor(private tourService: TourService , private tourLoaiService: TourLoaiService  ) {
+  constructor(private tourService: TourService , private tourLoaiService: TourLoaiService , private tourChiTietService : TourChiTietService ) {
     this.tourLoaiService.getTourLoais().toPromise().then((data)=>{
       this.LoaiTour = data;
     })
     this.tourService.getAll().toPromise().then((data)=>{
-      data.forEach(e=>{
-        this.tour.push(e)
-      })
+      // data.forEach(e=>{
+      //   this.tour.push(e)
+      // })
+      this.tour=data;
     })
   }
   getData() : void {
@@ -45,13 +49,25 @@ export class TourComponent implements OnInit {
       }, update: (values) => {
         return values;
       }, onUpdated: (key, values) => {
-        this.tourService.update({...key, ...values}).subscribe();
+        this.tourService.get(key.id).subscribe(value =>{
+          if(values.tour!=undefined) {
+            this.tourService.get(values.tour.id).toPromise().then(data => {
+              values.tour = data
+              this.tourService.update({...value, ...values}).subscribe();
+            })
+          }else{
+            this.tourService.update({...value, ...values}).subscribe();
+          }
+        })
       }, onInserted: (key, values) => {
-        this.tourService.create({...values}).subscribe();
+        console.log(key)
+        this.tourService.create({...key}).subscribe();
       }, remove: (values)=>{
         return values;
       }, onRemoved: (values)=>{
-        this.tourService.delete(values).subscribe();
+        console.log(values)
+        // this.tourService.delete(values).subscribe();
+
       }
     });
   }
--- src/app/services/tour-chi-tiet.service.ts
+++ src/app/services/tour-chi-tiet.service.ts
@@ -3,6 +3,8 @@ import {Task} from "../shared/model/Task";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {TourChiTiet} from "../shared/model/TourChiTiet";
+import {Tour} from "../shared/model/Tour";
+import {Khachhang} from "../shared/model/khachhang";
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -30,5 +32,12 @@ export class TourChiTietService {
     return this.http.put<TourChiTiet>(url, tourChiTiet, httpOptions);
   }
 
+  deleteTourChiTietByTourID(id: number): Observable<TourChiTiet>{
+    const url = `${this.apiUrl}/tourid/${id}`;
+    return this.http.delete<TourChiTiet>(url);
+  }
+  create(tourChiTiet: TourChiTiet): Observable<TourChiTiet>{
+    return this.http.post<TourChiTiet>(this.apiUrl, tourChiTiet, httpOptions);
+  }
 
 }
